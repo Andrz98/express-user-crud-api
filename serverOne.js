486f287eca365071e81f05da3a6c818da22a2b3b
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2024-12-30 22:46:31 +0100

    Server init

--- serverOne.js
+++ serverOne.js
@@ -1,2 +1,17 @@
 import express from 'express';
 const app = express();
+
+const port = 3000;
+
+// Middleware para registrar rutas (se ejecuta en todas las solicitudes)
+
+app.use((req, res, next) => {
+  console.info(`[INFO] ${req.method} ${req.path}`);
+  next(); // llamo a la siguiente fila de middleware en la pila
+});
+
+app.use(express.json()); //permite recibir y procesar datos en formato JSON desde el cliente
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
