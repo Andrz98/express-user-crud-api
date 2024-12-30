4369b18aab4884e66d397e56e476096880e036e7
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2024-12-30 22:37:44 +0100

    eslintConfig 1.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
-
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'], // Se ajustaron los tipos de archivos permitidos
+    languageOptions: {
+      globals: globals.browser, // Configuración del entorno global para navegadores
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules, // Reglas para React
+    },
+  },
 ];
